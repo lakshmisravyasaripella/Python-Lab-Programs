61733565b361d304e057a0ebe60b727d03374fa6
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-08-25 15:45:50 +0530

    Add files via upload

---
+++ Exp-4.3.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number"))
+print("The number separated with commas is:{:,}".format(n))
+'''
+Output:
+Enter a number10000000
+The number separated with commas is:10,000,000
+'''
