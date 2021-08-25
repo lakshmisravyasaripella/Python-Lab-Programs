01d7dba31dc5f30ec3f4b3443ac4de7ef15b68a5
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-08-25 14:13:06 +0530

    Add files via upload

---
+++ Exp-3.1.py
@@ -0,0 +1,19 @@
+n=int(input("Enter the traiangle height:"))
+for i in range(n):
+    for j in range(i):
+        print("*",end=" ")
+    print("\n")
+'''
+Output:
+Enter the traiangle height:5
+
+
+* 
+
+* * 
+
+* * * 
+
+* * * * 
+'''
+    
