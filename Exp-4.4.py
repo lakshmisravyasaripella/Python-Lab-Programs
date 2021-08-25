156de48b3d3bf777f6b17b0e81ad132a5b354290
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-08-25 16:12:16 +0530

    Add files via upload

---
+++ Exp-4.4.py
@@ -0,0 +1,20 @@
+exp=input("Enter algebric expression:")
+conv_exp=''
+for ch in exp:
+    if ch>='0' and ch<='9':
+        conv_exp=conv_exp+ch
+    elif ch=='(':
+        conv_exp=conv_exp+'*'+ch
+    elif ch>='a' and ch<='z' and conv_exp[-1]!='(':
+        conv_exp=conv_exp+'*'+ch
+    else:
+        conv_exp=conv_exp+ch
+print("Converted expression is:",conv_exp)
+'''
+Output:
+Enter algebric expression:3x+4y
+Converted expression is:3*x+4*y
+
+Enter algebric expression:3(x+5)
+Converted expression is:3*(x+5)
+'''
