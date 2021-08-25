d1556c41bf6aeaddd58302a4a2fa93be0119d8ef
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-08-25 15:39:17 +0530

    Add files via upload

---
+++ Exp-2.2.py
@@ -0,0 +1,14 @@
+name=input("Enter your name:")
+n=int(input("Enter the number of times the name to be printed:"))
+for i in range(n):
+    print(name)
+'''    
+0utput:
+Enter your name:Sravya
+Enter the number of times the name to be printed:5
+Sravya
+Sravya
+Sravya
+Sravya
+Sravya    
+'''
