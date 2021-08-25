08ce0d67953e3364739950ac66fe38459f8fcfc6
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-08-25 15:38:48 +0530

    Add files via upload

---
+++ Exp-1.2.py
@@ -0,0 +1,15 @@
+a=int(input("Enter number a"))
+b=int(input("Enter number b"))
+c=int(input("Enter number c"))
+total=a+b+c
+print("total=",total)
+average=(a+b+c)/3
+print("average=",average)
+'''
+Output:
+Enter number a9
+Enter number b3
+Enter number c6
+total= 18
+average= 6.0
+'''
