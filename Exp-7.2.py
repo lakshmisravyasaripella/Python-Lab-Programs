d08a6d9a70603cb186e25b7ce5d9806f621af1e2
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-09-06 21:22:27 +0530

    Add files via upload

---
+++ Exp-7.2.py
@@ -0,0 +1,18 @@
+def first_diff(str1, str2):
+    ms = min(len(str1), len(str2))
+    for i in range(ms):
+        if str1[i] != str2[i]: return i
+    return -1
+first_str = input("Enter String 1: ")
+second_str = input("Enter String 2: ")
+print(first_diff(first_str, second_str))
+'''
+Output:
+Enter String 1: python
+Enter String 2: python
+-1
+
+Enter String 1: python
+Enter String 2: pycharm
+2
+'''
