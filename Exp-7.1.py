5097d113055d13ca3c29343298f378828c02bd37
Lakshmi Sravya Saripella <84490807+lakshmisravyasaripella@users.noreply.github.com>
2021-09-06 21:21:45 +0530

    Add files via upload

---
+++ Exp-7.1.py
@@ -0,0 +1,9 @@
+def sum_digits(num):
+    return sum([int(d) for d in num])
+n = input("Enter an integer number: ")
+print("Sum of digits of",n,"is",sum_digits(n))
+'''
+Output:
+Enter an integer number: 12345
+Sum of digits of 12345 is 15
+'''
